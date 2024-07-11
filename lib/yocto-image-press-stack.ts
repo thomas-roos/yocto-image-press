318d1171445a35fbfcf3717e5e2ea260918f1553
Thomas Roos <throos@amazon.de>
2024-07-11 16:36:06 +0200

    working
    Download time: 25.85040283203125 seconds
    Execution time: 5.938045024871826 seconds
    Upload time: 25.447730541229248 seconds

--- lib/yocto-image-press-stack.ts
+++ lib/yocto-image-press-stack.ts
@@ -3,43 +3,41 @@ import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3_deployment from 'aws-cdk-lib/aws-s3-deployment';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as iam from 'aws-cdk-lib/aws-iam';
+import * as ecrAssets from 'aws-cdk-lib/aws-ecr-assets';
 import * as path from 'path';
 
 export class YoctoImagePressStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-
     // Create S3 bucket
     const bucket = new s3.Bucket(this, 'MyBucket');
 
     // Upload a file to the S3 bucket
-    new s3_deployment.BucketDeployment(this, 'DeployFile', {
-      sources: [s3_deployment.Source.asset(path.join(__dirname, '../data'))],
-      destinationBucket: bucket,
+    // new s3_deployment.BucketDeployment(this, 'DeployFile', {
+      // sources: [s3_deployment.Source.asset(path.join(__dirname, '../data'))],
+      // destinationBucket: bucket,
+    // });
+
+    // Define Docker containing wic tools
+    const dockerImageAsset = new ecrAssets.DockerImageAsset(this, 'MyDockerImageAsset', {
+      directory: path.join(__dirname, '../lambda'),
     });
 
-    // Layer containing wic tools
-    const lambdaLayer = new lambda.LayerVersion(this, 'MyLayer', {
-       code: lambda.Code.fromDockerBuild(path.join(__dirname, '../layer'))
-    });
-
-    // Lambda function to create the image
-    const myFunction = new lambda.Function(this, 'MyFunction', {
-      code: lambda.Code.fromAsset(path.join(__dirname, '../function')),
+    const myFunction = new lambda.DockerImageFunction(this, 'MyFunction', {
+      code: lambda.DockerImageCode.fromEcr(dockerImageAsset.repository,  {
+          tagOrDigest: dockerImageAsset.imageTag
+      }),
       environment: {
         BUCKET_NAME: bucket.bucketName,
       },
-      handler: 'lambda_function.lambda_handler',
       memorySize: 10240,
       ephemeralStorageSize: cdk.Size.mebibytes(10240),
-      timeout: cdk.Duration.seconds(600),
-      runtime: lambda.Runtime.FROM_IMAGE,
-      layers: [lambdaLayer], // Attach the layer to the Lambda function
-    });
+      timeout: cdk.Duration.seconds(600)
+    })
 
     // Grant the Lambda function read permissions to the S3 bucket
     bucket.grantReadWrite(myFunction);
+
   }
 }
